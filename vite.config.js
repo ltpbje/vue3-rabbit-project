2208fd3ea150bd747d36b85f0a6c301ef2d167c1
ltpbje <19836788252@163.com>
2024-03-02 09:39:04 +0800

    elementPlus主题色配置完成

--- vite.config.js
+++ vite.config.js
@@ -6,22 +6,37 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// 导入对应包
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    
+
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
-    }),
+      resolvers:[
+        // 1. 配置elementPLus采用sass样式配色系统
+        ElementPlusResolver({importStyle: "sass"}),
+      ],
+    })
+    
   ],
   resolve: {
     // 实际的路径转化
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2. 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
